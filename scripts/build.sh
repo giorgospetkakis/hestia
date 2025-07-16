b9528444f48aa5f7175183bc6296e4108c16eddf
Giorgos Petkakis <85838659+giorgospetkakis@users.noreply.github.com>
2025-07-16 13:09:06 +0200

    clean up commands

--- scripts/build.sh
+++ scripts/build.sh
@@ -36,8 +36,8 @@ if ! command -v flutter &> /dev/null; then
     git clone https://github.com/flutter/flutter.git -b stable --depth 1
     export PATH="$PWD/flutter/bin:$PATH"
     flutter doctor
-    flutter/bin/flutter clean
-    flutter/bin/flutter config --enable-web
+    flutter clean
+    flutter config --enable-web
 fi
 
 # Install Flutter dependencies and build
