237357b4fa6919515875ef82ae9f2bb14b5ded37
Giorgos Petkakis <85838659+giorgospetkakis@users.noreply.github.com>
2025-07-16 13:10:45 +0200

    Change dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -36,8 +36,10 @@ if ! command -v flutter &> /dev/null; then
     git clone https://github.com/flutter/flutter.git -b stable --depth 1
     export PATH="$PWD/flutter/bin:$PATH"
     flutter doctor
+    cd frontend
     flutter clean
     flutter config --enable-web
+    cd ..
 fi
 
 # Install Flutter dependencies and build
